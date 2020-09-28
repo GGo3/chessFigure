971cba69d8642f52f1225a6f413f88f080f28ba4
GGo3 <voloshynmv@gmail.com>
2020-09-28 13:09:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// 1) Сделать шахматную доску, расставить фигуры. Использовать только JS, на html странице только один div. Стили можно описать в SCSS. 
+
+let boardColumns = [];
+const сhessBoard = document.querySelector('.main');
+const chessFigures = [20, 22, 21, 19, 18, 21, 22, 20, 23];
+let str = '';
+let color = '';
+
+for (let i = 0; i < 8; i++) {
+  let boardRow = [];
+  
+  for ( let k = 0; k < 8; k++) {
+    boardRow[k] = '';  
+    if (i === 0 || i === 7) {
+      boardRow[k] = `&#98${chessFigures[k]}`;
+      if (i === 7) {
+        color = `style="color: #fff;"`;
+      }
+    } else if (i === 1 || i === 6) {
+      boardRow[k] = `&#98${chessFigures[8]}`;
+      if (i === 6) {
+        color = `style="color: #fff;"`;
+      }
+    } else {
+      boardRow[k] = '';
+    }
+    if (((k + i) % 2) != 0 ) {
+      
+      str = `${str}<div class="black" ${color}>${boardRow[k]}</div>`;
+    } else {
+      str = `${str}<div class="white" ${color}>${boardRow[k]}</div>`;
+       
+    }
+  }
+  boardColumns[i] = boardRow;
+}
+сhessBoard.innerHTML = str;
